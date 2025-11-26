2705146d161e5c6d2e633d72aef84d04adf0d036
Supun Ravindu <supunravindu173@gmail.com>
2025-11-26 19:02:37 +0530

    lib main.dart

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'screens/sign_in_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Crop Select',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        fontFamily: 'Poppins',
+      ),
+      home: const SignInScreen(),
+    );
+  }
+}
